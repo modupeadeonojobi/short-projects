a7d2b79a595d9a6d094a34ff545736697347cb50
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2021-02-25 13:08:24 +0100

    Add faqbox files

---
+++ faqBoxes/script.js
@@ -0,0 +1,8 @@
+const toggles = document.querySelectorAll('.faq-toggle');
+
+
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentElement.classList.toggle('active');
+    })
+})
