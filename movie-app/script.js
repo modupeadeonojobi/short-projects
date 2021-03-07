8b653feb8c25adffbfb662095b2847b9473e27c7
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2021-03-07 14:25:49 +0100

    Add movie-app style file

--- movie-app/script.js
+++ movie-app/script.js
@@ -1,3 +1,33 @@
-const API_URL = '';
-const IMG_PATH = '';
-const SEARCH_URL = '';
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
+
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
+
+// GET INITIAL MOVIES
+getMovie(API_URL);
+
+async function getMovie(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    console.log(data);
+}
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const searchTerm = search.value;
+
+    if (searchTerm && searchTerm !== '') {
+        getMovie(SEARCH_API + searchTerm);
+        search.value = '';
+    } else {
+        window.location.reload();
+
+    }
+
+})
